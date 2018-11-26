f27dd47b020ea205fc902c7eaeb18e64bc3f5a60
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-11-25 22:08:14 -0300

    Initial commit

---
+++ app/src/main/java/com/tecnologia/aquiles/broadcastreceiverinitcel/MainActivity.java
@@ -0,0 +1,23 @@
+package com.tecnologia.aquiles.broadcastreceiverinitcel;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver = new MyReceiver();
+    IntentFilter intentFilter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_BOOT_COMPLETED);
+
+        this.registerReceiver(receiver, intentFilter);
+    }
+}
---
+++ app/src/main/java/com/tecnologia/aquiles/broadcastreceiverinitcel/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.tecnologia.aquiles.broadcastreceiverinitcel;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, " MyBootCompleteReceiver: System is Ready to Use.",
+                Toast.LENGTH_LONG).show();
+
+    }
+}
